2c5665b0031429725c0b062f54e6f8fd974c3bbc
brunoliveirag <brunoliveiragomesz@gmail.com>
2024-04-14 15:15:15 -0300

    implement navigations

--- meau-expo/app/_layout.tsx
+++ meau-expo/app/_layout.tsx
@@ -1,11 +1,12 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { Pressable } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -14,7 +15,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'index',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -50,9 +51,17 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+        <Stack.Screen name="index" options={{
+        headerTitle: "Página Principal"
+        }}/>
+        <Stack.Screen name="(tabs)/telaAutenticacao"/>
+        <Stack.Screen name="(tabs)/telaLoginUsuario" options={{
+        headerTitle: "Login"
+        }}/>
+        <Stack.Screen name="(tabs)/telaCadastroUsuario" options={{
+        headerTitle: "Cadastro"
+        }}/>
+    </Stack>
     </ThemeProvider>
   );
 }
