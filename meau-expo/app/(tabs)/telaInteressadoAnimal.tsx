123256da1a0c8599e71b2e23485bb2c2c8127415
Gabriel Brito de França <gabrielbfranca@gmail.com>
2024-08-26 00:58:42 -0300

    criando notificação push para desenvolvimento

--- meau-expo/app/(tabs)/telaInteressadoAnimal.tsx
+++ meau-expo/app/(tabs)/telaInteressadoAnimal.tsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView, Image, TouchableOpacity, Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from 'expo-constants';
 
 const images = [
   "https://wildwoodvetclinic.com/wp-content/uploads/2020/08/WVC-newsletter-graphics-aug2020-BLOG.png",
@@ -13,17 +17,113 @@ const getRandomDogName = () => {
   return dogNames[index];
 };
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 const TelaInteressadoAnimal = () => {
   const [liked, setLiked] = useState(images.map(() => false));
   const [names] = useState(images.map(() => getRandomDogName()));
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    console.log("Registering for push notifications...");
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        if (token) {
+          console.log("token: ", token);
+          setExpoPushToken(token.data); // Only set if the token is defined
+        }
+        else {
+          console.log("No token received");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      });
+      console.log(token);
+    } else {
+      alert("Must use physical device for Push Notifications");
+    }
+
+    return token;
+  }
+
+  const sendNotification = async (message: any) => {
+    if (!expoPushToken) {
+      console.log("No push token available");
+      return;
+    }
+
+    console.log("Sending push notification...");
+
+    message.to = expoPushToken;
+
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        host: "exp.host",
+        accept: "application/json",
+        "accept-encoding": "gzip, deflate",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  };
 
   const toggleLike = (index: number) => {
     setLiked((prevLiked) => {
       const newLiked = [...prevLiked];
       newLiked[index] = !newLiked[index];
+
+      // Send notification if image is liked
+      if (newLiked[index]) {
+        const message = {
+          sound: "default",
+          title: "Someone liked your pet!",
+          body: `${names[index]} has a new admirer!`,
+        };
+        sendNotification(message);
+      }
+
       return newLiked;
     });
   };
+
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
